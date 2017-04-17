d361b2e2e65b03d164b8c9145ef6562934ed3522
petrero <petrero@gmail.com>
2017-04-17 17:12:30 +0300

    Copy code from branch 9

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -8,10 +8,74 @@ use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use KnpU\CodeBattle\Model\Programmer;
 
 class ProgrammerController extends BaseController {
   protected function addRoutes(ControllerCollection $controllers) {
-    // $controllers->post('/api/programmers', array($this, 'newAction'));
+    $controllers->post('/api/programmers', array($this, 'newAction'));
+    $controllers->get('/api/programmers', array($this, 'listAction'));
+    $controllers->get('/api/programmers/{nickname}', array($this, 'showAction'))
+      ->bind('api_programmers_show');
+  }
+  
+  public function newAction(Request $request){
+    $data = json_decode($request->getContent(), true);
+    
+    $programmer = new Programmer();
+    $programmer->nickname = $data['nickname'];
+    $programmer->avatarNumber = $data['avatarNumber'];
+    $programmer->tagLine = $data['tagLine'];
+    $programmer->userId = $this->findUserByUsername('weaverryan')->id;
+    
+    $this->save($programmer);
+    
+    $url = $this->generateUrl('api_programmers_show', array(
+      'nickname' => $programmer->nickname
+    ));
+    
+    $response = new Response('It worked! Trust me, I\'m an API', 201);
+    $response->headers->set('Location', $url);
+    
+    return $response;
+  }
+  
+  public function showAction($nickname){
+    $programmer = $this->getProgrammerRepository()
+      ->findOneByNickname($nickname);
+    
+    if (!$programmer){
+      $this->throw404('Oh no! This programmer has deserted! we\'ll send a search party!');  
+    }
+    
+    $data = $this->serializeProgrammer($programmer);
+    
+    $response = new Response(json_encode($data), 200);
+    $response->headers->set('Content-Type', 'application/json');
+    
+    return $response;
   }
 
+  public function listAction(){
+    $programmers = $this->getProgrammerRepository()
+      ->findAll();
+    
+    $data = array('programmers' => array());
+    foreach ($programmers as $programmer){
+      $data['programmers'][] = $this->serializeProgrammer($programmer); 
+    }
+    
+    $response = new Response(json_encode($data), 200);
+    $response->headers->set('Content-Type', 'application/json');
+    
+    return $response;
+  }
+  
+  private function serializeProgrammer(Programmer $programmer) {
+    return  array(
+      'nickname' => $programmer->nickname,
+      'avatarNumber' => $programmer->avatarNumber,
+      'powerLevel' => $programmer->powerLevel,
+      'tagLine' => $programmer->tagLine  
+    ); 
+  }
 }
--- testing.php
+++ testing.php
@@ -5,9 +5,29 @@ require __DIR__.'/vendor/autoload.php';
 use Guzzle\Http\Client;
 
 // create our http client (Guzzle)
-$client = new Client('http://localhost:8000', array(
+$client = new Client('http://localhost:80', array(
     'request.options' => array(
         'exceptions' => false,
     )
 ));
 
+$nickname = 'ObjectOrienter'.rand(0, 999);
+$data = array(
+  'nickname' => $nickname,
+  'avatarNumber' => 5,
+  'tagLine' => 'a test dev!'  
+);
+
+
+$request = $client->post('/api/programmers', null, json_encode($data));
+$response = $request->send();
+
+$url = $response->getHeader('Location');
+$request = $client->get($url);
+$response = $request->send();
+
+$request = $client->get('/api/programmers');
+$response = $request->send();
+
+echo $response;
+echo "\n\n";
