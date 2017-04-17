ccb6d6aee4d7050294f40411754800585fe4eccc
petrero <petrero@gmail.com>
2017-04-17 15:42:03 +0300

    Copy code from branch 6

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -8,10 +8,28 @@ use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use KnpU\CodeBattle\Model\Programmer;
 
 class ProgrammerController extends BaseController {
   protected function addRoutes(ControllerCollection $controllers) {
-    // $controllers->post('/api/programmers', array($this, 'newAction'));
+    $controllers->post('/api/programmers', array($this, 'newAction'));
+  }
+  
+  public function newAction(Request $request){
+    $data = json_decode($request->getContent(), true);
+    
+    $programmer = new Programmer();
+    $programmer->nickname = $data['nickname'];
+    $programmer->avatarNumber = $data['avatarNumber'];
+    $programmer->tagLine = $data['tagLine'];
+    $programmer->userId = $this->findUserByUsername('weaverryan')->id;
+    
+    $this->save($programmer);
+    
+    $response = new Response('It worked! Trust me, I\'m an API', 201);
+    $response->headers->set('Location', '/some/programmer/url');
+    
+    return $response;
   }
 
 }
