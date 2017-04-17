b1ced5e762372d0430c5be016d019c6c0e0df4dd
petrero <petrero@gmail.com>
2017-04-17 15:35:34 +0300

    Location Header
    And when we use the 201 status code, there’s another rule: include a Location header that points to the new resource. Hmm, we don’t have an endpoint to get a single programmer representation yet. To avoid angering the RESTful elders, let’s add a location header, and just fake the URL for now.
    
    If you stop and think about it, this is how the web works. When we submit a form to create a programmer, the server returns a redirect that takes us to the page to view it. In an API, the status code is 201 instead of 301 or 302, but the server is trying to help show us the way in both cases.
    
    Try the final product out in our test script:
    
    php testing.php
    
    HTTP/1.1 201 Created
    ...
    Location: /some/programmer/url
    Content-Type: text/html; charset=UTF-8
    
    It worked. Believe me - I'm an API
    Other than the random text we’re still returning, this endpoint is looking great. Now to GET a programmer!

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -26,7 +26,10 @@ class ProgrammerController extends BaseController {
     
     $this->save($programmer);
     
-    return 'It worked! Trust me, I\'m an API';
+    $response = new Response('It worked! Trust me, I\'m an API', 201);
+    $response->headers->set('Location', '/some/programmer/url');
+    
+    return $response;
   }
 
 }
