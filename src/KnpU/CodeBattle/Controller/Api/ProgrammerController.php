f0f93357737d15dc8731020c6c33017b46c433dd
petrero <petrero@gmail.com>
2017-04-17 20:03:58 +0300

    Our tests are passing, but we’re doing a bad job: I’d created duplicated code in newAction and updateAction!
    Let’s redeem ourselves! Create a new private function called handleRequest and copy the code into it that reads the request body and sets the data on the Programmer. Cool! Now we can just call this from newAction and updateAction.
    
    Re-run the tests to see if we broke anything:
    
    php vendir/bin/behat

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -20,15 +20,9 @@ class ProgrammerController extends BaseController {
   }
   
   public function newAction(Request $request){
-    $data = json_decode($request->getContent(), true);
     
     $programmer = new Programmer();
-    $programmer->nickname = $data['nickname'];
-    $programmer->avatarNumber = $data['avatarNumber'];
-    $programmer->tagLine = $data['tagLine'];
-    $programmer->userId = $this->findUserByUsername('weaverryan')->id;
-    
-    $this->save($programmer);
+    $this->handleRequest($request, $programmer);
     
     $url = $this->generateUrl('api_programmers_show', array(
       'nickname' => $programmer->nickname
@@ -79,16 +73,7 @@ class ProgrammerController extends BaseController {
       $this->throw404('Oh no! This programmer has deserted! we\'ll send a search party!');  
     }
     
-    $data = json_decode($request->getContent(), true);
-    
-    
-    $programmer->nickname = $data['nickname'];
-    $programmer->avatarNumber = $data['avatarNumber'];
-    $programmer->tagLine = $data['tagLine'];
-    $programmer->userId = $this->findUserByUsername('weaverryan')->id;
-    
-    $this->save($programmer);
-    
+    $this->handleRequest($request, $programmer);
     
     $data = $this->serializeProgrammer($programmer);
     
@@ -105,5 +90,16 @@ class ProgrammerController extends BaseController {
       'powerLevel' => $programmer->powerLevel,
       'tagLine' => $programmer->tagLine  
     ); 
+  }
+  
+  private function handleRequest(Request $request, Programmer $programmer){
+    $data = json_decode($request->getContent(), true);
+    
+    $programmer->nickname = $data['nickname'];
+    $programmer->avatarNumber = $data['avatarNumber'];
+    $programmer->tagLine = $data['tagLine'];
+    $programmer->userId = $this->findUserByUsername('weaverryan')->id;
+    
+    $this->save($programmer); 
   }
 }
