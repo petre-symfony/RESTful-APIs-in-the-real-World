80b18b8819de5fde197a57be7381180233609e13
petrero <petrero@gmail.com>
2017-04-17 16:37:57 +0300

    Copy code from branch 8

--- testing.php
+++ testing.php
@@ -5,9 +5,26 @@ require __DIR__.'/vendor/autoload.php';
 use Guzzle\Http\Client;
 
 // create our http client (Guzzle)
-$client = new Client('http://localhost:8000', array(
+$client = new Client('http://localhost:80', array(
     'request.options' => array(
         'exceptions' => false,
     )
 ));
 
+$nickname = 'ObjectOrienter'.rand(0, 999);
+$data = array(
+  'nickname' => $nickname,
+  'avatarNumber' => 5,
+  'tagLine' => 'a test dev!'  
+);
+
+
+$request = $client->post('/api/programmers', null, json_encode($data));
+$response = $request->send();
+
+$url = $response->getHeader('Location');
+$request = $client->get($url);
+$response = $request->send();
+
+echo $response;
+echo "\n\n";
