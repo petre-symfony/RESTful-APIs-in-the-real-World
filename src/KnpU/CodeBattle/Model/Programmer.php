2b42990b0898f794bf31b910c721c84f4088e34e
petrero <petrero@gmail.com>
2017-04-18 21:27:44 +0300

    Copy code from branch 21

--- src/KnpU/CodeBattle/Model/Programmer.php
+++ src/KnpU/CodeBattle/Model/Programmer.php
@@ -10,6 +10,9 @@ class Programmer {
   /* All public properties are persisted */
   public $id;
 
+  /**
+   * @Assert\NotBlank(message="Enter a clever nickname!") 
+   */
   public $nickname;
 
   /**
