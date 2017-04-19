c334aebe15c3769a6e787f2646b43630c9f4430d
petrero <petrero@gmail.com>
2017-04-19 19:06:51 +0300

    Copy code from branch 23

--- src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
+++ src/KnpU/CodeBattle/Controller/Api/ProgrammerController.php
@@ -8,10 +8,158 @@ use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use KnpU\CodeBattle\Model\Programmer;
+use KnpU\CodeBattle\Api\ApiProblem;
 
 class ProgrammerController extends BaseController {
   protected function addRoutes(ControllerCollection $controllers) {
-    // $controllers->post('/api/programmers', array($this, 'newAction'));
+    $controllers->post('/api/programmers', array($this, 'newAction'));
+    $controllers->get('/api/programmers', array($this, 'listAction'));
+    $controllers->get('/api/programmers/{nickname}', array($this, 'showAction'))
+      ->bind('api_programmers_show');
+    $controllers->put('/api/programmers/{nickname}', array($this, 'updateAction'));
+    $controllers->patch('/api/programmers/{nickname}', array($this, 'updateAction'))
+      ->method('PATCH');
+    $controllers->delete('/api/programmers/{nickname}', array($this, 'deleteAction'));
+  }
+  
+  public function newAction(Request $request){
+    
+    $programmer = new Programmer();
+    $this->handleRequest($request, $programmer);
+    
+    $errors = $this->validate($programmer);
+    if (!empty($errors)){
+      return $this->handleValidationResponse($errors);
+    }
+    
+    $this->save($programmer); 
+    
+    $url = $this->generateUrl('api_programmers_show', array(
+      'nickname' => $programmer->nickname
+    ));
+    
+    $data = $this->serializeProgrammer($programmer);
+    
+    $response = new JsonResponse($data, 201);
+    $response->headers->set('Location', $url);
+    
+    return $response;
+  }
+  
+  public function showAction($nickname){
+    $programmer = $this->getProgrammerRepository()
+      ->findOneByNickname($nickname);
+    
+    if (!$programmer){
+      $this->throw404('Oh no! This programmer has deserted! we\'ll send a search party!');  
+    }
+    
+    $data = $this->serializeProgrammer($programmer);
+    
+    $response = new JsonResponse($data, 200);
+    
+    return $response;
   }
 
+  public function listAction(){
+    $programmers = $this->getProgrammerRepository()
+      ->findAll();
+    
+    $data = array('programmers' => array());
+    foreach ($programmers as $programmer){
+      $data['programmers'][] = $this->serializeProgrammer($programmer); 
+    }
+    
+    $response = new JsonResponse($data, 200);
+    
+    return $response;
+  }
+  
+  public function updateAction(Request $request, $nickname){
+    $programmer = $this->getProgrammerRepository()
+      ->findOneByNickname($nickname);
+    
+    if (!$programmer){
+      $this->throw404('Oh no! This programmer has deserted! we\'ll send a search party!');  
+    }
+    
+    $this->handleRequest($request, $programmer);
+    
+    $errors = $this->validate($programmer);
+    if (!empty($errors)){
+      return $this->handleValidationResponse($errors);
+    }
+    $this->save($programmer); 
+    
+    $data = $this->serializeProgrammer($programmer);
+    
+    $response = new JsonResponse($data, 200);
+    
+    return $response;
+  }
+  
+  public function deleteAction($nickname){
+    $programmer = $this->getProgrammerRepository()
+      ->findOneByNickname($nickname);
+    
+    if($programmer){
+      $this->delete($programmer);
+    }
+    
+    return new Response(null, 204);
+  }
+  private function serializeProgrammer(Programmer $programmer) {
+    return  array(
+      'nickname' => $programmer->nickname,
+      'avatarNumber' => $programmer->avatarNumber,
+      'powerLevel' => $programmer->powerLevel,
+      'tagLine' => $programmer->tagLine  
+    ); 
+  }
+  
+  private function handleRequest(Request $request, Programmer $programmer){
+    $data = json_decode($request->getContent(), true);
+    $isNew = !$programmer->id;
+    
+    if ($data === null){
+      throw new Exception('Invalid JSON!!!!', $request->getContent());
+    }
+    
+    
+    $apiProperties = array('avatarNumber', 'tagLine');
+    if($isNew){
+      $apiProperties[] = 'nickname';  
+    }
+    
+    foreach ($apiProperties as $property){
+      //if PATCH and the field isn't send, just skip it!
+      if($request->isMethod('PATCH') && !isset($data[$property])){
+        continue;  
+      }
+      
+      $val = isset($data[$property]) ? $data[$property] : null;
+      $programmer->$property = $val;
+    }
+    
+    $programmer->userId = $this->findUserByUsername('weaverryan')->id;
+    
+  }
+  
+  private function handleValidationResponse(array $errors) {
+    
+    $apiProblem = new ApiProblem(
+      400,
+      ApiProblem::TYPE_VALIDATION_ERROR  
+    );
+    $apiProblem->set('errors', $errors);
+      
+    $response = new JsonResponse(
+      $apiProblem->toArray(), 
+      $apiProblem->getStatusCode()
+    );
+    $response->headers->set('Content-Type', 'application/problem+json');
+    
+    return $response; 
+  }
 }
